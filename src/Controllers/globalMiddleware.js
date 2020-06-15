01014a4e9150a7820019c3906b15cf69ba0370a9
Luiz Matos <luizmatos88@yahoo.com.br>
2020-06-15 19:46:50 -0300

    Transformando app em classes

--- src/Controllers/globalMiddleware.js
+++
@@ -1,5 +0,0 @@
-module.exports = (req, res, next) => {
-  console.time("Request")
-  console.log(`Method: ${req.method}; URL: ${req.url}`)
-  next()
-}
--- src/Controllers/middlewareUser.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-  if (!req.body.name) {
-    return res.status(400).json({ error: "User name is required" })
-  }
-  return next()
-}
