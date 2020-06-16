8cb26021ccbef76c61d9103cc4029a1c1bd8c4b0
Luiz Matos <luizmatos88@yahoo.com.br>
2020-06-16 17:34:45 -0300

    Criado migrate do banco de dados postgress Configurado  dependências

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
-import { Router } from "express"
+import { Router } from 'express'
+
+import User from './app/models/User'
 
 const routes = Router()
 
-routes.get("/", (req, res) => {
-  return res.json({ ok: true })
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Luiz Eduardo',
+    email: 'luizmatosedu@gmail.com',
+    passowrd_hash: '12345',
+  })
+
+  return res.json(user)
 })
 
 export default routes
