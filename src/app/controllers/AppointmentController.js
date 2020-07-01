bef86dcb73f7900f4a5ea4d703d64c228ac5dd42
Luiz Matos <luizmatos88@yahoo.com.br>
2020-06-30 21:09:37 -0300

    Adicionado date-fns para controlar o horário de agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,6 +1,7 @@
+import * as Yup from 'yup'
+import { startOfHour, parseISO, isBefore } from 'date-fns'
 import User from '../models/User'
 import Appointment from '../models/Appointment'
-import * as Yup from 'yup'
 
 class AppointmetController {
   async store(req, res) {
@@ -27,10 +28,28 @@ class AppointmetController {
         .json({ error: 'You can only create appointmets with providers' })
     }
 
+    const hourStart = startOfHour(parseISO(date))
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past date are not permitted' })
+    }
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    })
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' })
+    }
+
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date,
+      date: hourStart,
     })
     return res.json(appointment)
   }
