32beb16721239310a031d45ab7af6146c1c03275
Luiz Matos <luizmatos88@yahoo.com.br>
2020-07-06 20:35:19 -0300

    Email enviado para o provider quando um agendamento é cancelado

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,6 +6,7 @@ import User from '../models/User'
 import File from '../models/File'
 import Appointment from '../models/Appointment'
 import Notification from '../schemas/Notification'
+import Mail from '../../lib/Mail'
 
 class AppointmetController {
   async index(req, res) {
@@ -87,15 +88,12 @@ class AppointmetController {
     })
 
     // Notification for provider
-
     const user = await User.findByPk(req.userId)
-
     const formattedDate = format(
       hourStart,
       "'dia' dd 'de' MMMM', às' H:mm'h'",
       { locale: pt }
     )
-
     await Notification.create({
       content: `Novo agendamento de ${user.name} para ${formattedDate}`,
       user: provider_id,
@@ -104,7 +102,15 @@ class AppointmetController {
     return res.json(appointment)
   }
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id)
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+      ],
+    })
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({
         error: 'You do not have permission to cancel this appointment',
@@ -119,6 +125,11 @@ class AppointmetController {
     }
     appointment.canceled_at = new Date()
     await appointment.save()
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      text: 'Você tem um novo cancelamento',
+    })
     return res.json(appointment)
   }
 }
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'd36e685b51376f',
+    pass: 'cd1827b3f789e8',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+}
