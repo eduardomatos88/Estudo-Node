1f5924937caee540aba1d16c9ba59cc2ea3fe246
Luiz Matos <luizmatos88@yahoo.com.br>
2020-07-06 21:01:13 -0300

    Adicionado template de email de cancelamento de agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -109,6 +109,11 @@ class AppointmetController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
       ],
     })
     if (appointment.user_id !== req.userId) {
@@ -128,7 +133,14 @@ class AppointmetController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
-      text: 'Você tem um novo cancelamento',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
     })
     return res.json(appointment)
   }
